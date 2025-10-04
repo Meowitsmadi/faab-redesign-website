c78fae5ac21571082484b08af2b4c431336073fb
gaileen <gail.andal@icloud.com>
2025-10-04 16:52:30 -0400

    Connect API posts for react display. Add Swagger UI for backend testing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,21 +1,27 @@
 import '../styles/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { usePosts } from "../hooks/FetchPosts";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
+import ShopContext from './ShopContext'; 
 
 function App() {
-  
+  const { posts, loading } = usePosts();
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <>
-      <Navbar />
-      <main className="page">
-        <Outlet />
-      </main>
-      <Footer />
-    
+      <ShopContext.Provider value={{ posts }}>
+        <Navbar />
+        <div className="page">
+          <Outlet />
+        </div>
+        <Footer />
+      </ShopContext.Provider>
     </>
   )
 }
 
-export default App
+export default App;
--- client/src/components/BlogPost.jsx
+++ client/src/components/BlogPost.jsx
@@ -1,10 +1,10 @@
 
 export default function BlogPost({ post }) {
     return (
-        <div>
+        <div className="bg-light p-3 mb-3 rounded">
             <h2>{post.title}</h2>
-            <p><strong>Published:</strong> {new Date(post.published).toLocaleString()}</p>
-            <p><strong>Author:</strong> {post.author.displayName}</p>
+            {/* <p><strong>Published:</strong> {new Date(post.published).toLocaleString()}</p>
+            <p><strong>Author:</strong> {post.author.displayName}</p> */}
             <p>{post.content}</p>
         </div>
     );
