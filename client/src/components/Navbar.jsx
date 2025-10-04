a9d3a4cb25a89d85d22580ceef1fc75031b9a504
gaileen <gail.andal@icloud.com>
2025-10-04 10:56:36 -0400

    Update imports in routes for About and Contact

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,13 +30,13 @@ export default function Navbar() {
               <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/about-us">About Us</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/schedule">Schedule</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact-us">Contact Us</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/blog-archive">Blog Archive</Link>
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,9 +1,9 @@
 import App from "./components/App";
 import Error from "./components/pages/Error";
 import Home from "./components/pages/Home";
-// import About from "./components/About";
+import AboutUs from "./components/pages/AboutUs";
 import Schedule from "./components/pages/Schedule";
-// import ContactUs from "./components/ContactUs";
+import Contact from "./components/pages/Contact";
 import BlogArchive from "./components/pages/BlogArchive";
 
 const routes = [
@@ -13,9 +13,9 @@ const routes = [
         errorElement: <Error/>,   
         children: [
             { index: true, element: <Home /> },
-            // { path: "/about", element: <About /> },
+            { path: "/about-us", element: <AboutUs /> },
             { path: "/schedule", element: <Schedule /> },
-            // { path: "/contact-us", element: <ContactUs /> },
+            { path: "/contact", element: <Contact /> },
             { path: "/blog-archive", element: <BlogArchive /> },
             // BELOW IS PLACEHOLDER. NOT SURE IF WE'RE COMBINING PICS + VIDS
             // { path: "/gallery", element: <Gallery /> }, 
