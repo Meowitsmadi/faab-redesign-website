20ea246b103c8dae50c361baa9125c3fe436a6e8
Gerlin Bongato <mikaelabongato@gmail.com>
2025-10-04 08:48:33 -0400

    Add ScheduleWigdet and data file for weekly mass schedule

---
+++ client/src/data/masses.js
@@ -0,0 +1,4 @@
+export const weeklyMasses = [
+  { id: 'mon', day: 'Mon', time: '9:00 AM', lang: 'ENG', parish: 'St. Joseph Church', city: 'Malden' },
+  { id: 'tue', day: 'Tue', time: '12:00 PM', lang: 'ENG/FIL', parish: 'La Salette Shrine', city: 'Attleboro' },
+];
