5d8b9a90fa69cdabf179f715b67d35934aefaeb5
gaileen <gail.andal@icloud.com>
2025-10-05 14:47:34 -0400

    Added photo gallery links sample

--- client/src/components/pages/Gallery.jsx
+++ client/src/components/pages/Gallery.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
+    <div>
+      <h2>Photo Gallery</h2>
+      <div>
+        <a href="https://photos.app.goo.gl/HYmKkPbN7jT5kiFC8" target="_blank">Mass of Remembrance 2021</a>
+      </div>
+      <div>
+        <a href="https://photos.app.goo.gl/BRMWPn6d2fKJwpv69">Family Fun Night 2019</a>
+      </div>
+      <div>
+        <a href="https://photos.app.goo.gl/scAliTXOgRKmlh6E3">Easter Celebration Malden 2018</a>
+      </div>
+      <div>
+        <a href="https://photos.app.goo.gl/nIiSJQBXjaHMRs672">Mass for Typhoon Haiyan Victims</a>
+      </div>
+      <div>
+        <a href="https://photos.app.goo.gl/TbYIsfyC7Tq9q2Q73">Mass with Cardinal O'Malley</a>
+      </div>
+      <div>
+        <a href="https://photos.app.goo.gl/cQQO44xIpfLd39sG2">Family Fun Night 2013</a>
+      </div>
+      <div>
+        <a href="https://photos.app.goo.gl/5NtSYVdgE3z65V3s2">Christmas in Malden 2012</a>
+      </div>
+    </div>
+  );
 }
 
-export default Gallery
+export default Gallery;
