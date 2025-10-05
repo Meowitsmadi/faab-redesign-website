9d0bc2567b1f2754ec03998f9453a7ad78ad5ce6
GeraldineB <deemaine628@gmail.com>
2025-10-05 14:36:02 -0400

    changed the color of the nav bar to navy

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 import logo from "../assets/pics/ApostolateLogo.png";
 
 export default function Navbar() {
+  const navy = "#052a4e";
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light position-sticky top-0 z-index-9999" style={{ backgroundColor: "white" }}>
+    <nav
+      className="navbar navbar-expand-lg navbar-dark position-sticky top-0 z-index-9999"
+      style={{ backgroundColor: navy }}
+    >
       <div className="container-fluid">
         <Link to="/" className="navbar-brand d-flex align-items-center gap-2">
           <img
@@ -12,12 +17,12 @@ export default function Navbar() {
             style={{ height: 40, width: "auto", objectFit: "contain" }}
             className="img-fluid"
           />
-          <span className="fw-semibold d-none d-sm-inline fs-6 fst-italic">
+          <span className="fw-semibold d-none d-sm-inline fs-6 fst-italic text-white">
             Filipino Apostolate of the Archdiocese of Boston
           </span>
         </Link>
 
-        {/* Toggler button aligned right */}
+        {/* Toggler */}
         <button
           className="navbar-toggler order-lg-3"
           type="button"
@@ -26,47 +31,37 @@ export default function Navbar() {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          style={{ borderColor: "rgba(255,255,255,.25)" }}
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon" />
         </button>
 
-        {/* Navbar links, collapse on small screens, aligned right */}
-        <div className="collapse navbar-collapse justify-content-end order-lg-2" id="navbarNav">
+        {/* Links */}
+        <div
+          className="collapse navbar-collapse justify-content-end order-lg-2"
+          id="navbarNav"
+        >
           <ul className="navbar-nav mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
-                Home
-              </Link>
+              <Link className="nav-link text-white" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about-us">
-                About Us
-              </Link>
+              <Link className="nav-link text-white" to="/about-us">About Us</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/schedule">
-                Schedule
-              </Link>
+              <Link className="nav-link text-white" to="/schedule">Schedule</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
+              <Link className="nav-link text-white" to="/contact">Contact</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/blog-archive">
-                Blog Archive
-              </Link>
+              <Link className="nav-link text-white" to="/blog-archive">Blog Archive</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/members">
-                Members
-              </Link>
+              <Link className="nav-link text-white" to="/members">Members</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/gallery">
-                Gallery
-              </Link>
+              <Link className="nav-link text-white" to="/gallery">Gallery</Link>
             </li>
           </ul>
         </div>
